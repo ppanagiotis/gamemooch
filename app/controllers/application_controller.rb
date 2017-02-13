2195b127b8820c471855b560bd20906e9ea25086
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-02-14 01:12:02 +0200

    Make forms a little more ugly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
 end
