1e917f1a11b543947db679c9161c5c15bf289943
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-02-18 21:00:09 +0200

    Add user delete games action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
   def games
     @user = current_user
     @games = @user.games.paginate(page: params[:page], per_page: 20)
+
+  def delete_games
+    response = params[:selectImage]
+    if response
+      response.each do |game|
+        game = json(game)
+        id = game['id']
+        @game = Game.find(id)
+        @game.destroy
+      end
+      redirect_to request.env["HTTP_REFERER"]
+    else
+      flash[:error] = "Please select a game"
+      redirect_to request.env["HTTP_REFERER"]
+    end
   end
+
 end
