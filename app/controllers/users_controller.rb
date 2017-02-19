426f90d0f76336446275f6d638d41e7264ee1f73
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-02-19 15:52:33 +0200

    Add requested by other form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,26 @@ class UsersController < ApplicationController
   def games
     @user = current_user
     @games = @user.games.paginate(page: params[:page], per_page: 20)
+  def requested_games
+    response = params[:pendingImage]
+    if response
+      response.each do |game|
+        game = json(game)
+        id = game['id']
+        @game = Game.find(id)
+        if params[:commit] == "Decline"
+          @game.mooch_user = nil
+        else
+          @game.mooched = true
+        end
+        @game.save
+      end
+      redirect_to request.env["HTTP_REFERER"]
+    else
+      flash[:error] = "Please select a game"
+      redirect_to request.env["HTTP_REFERER"]
+    end
+  end
 
   def delete_games
     response = params[:selectImage]
