0df34e16ea4d7502929cbe45bd5bc4eed9b0838b
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-02-24 02:09:56 +0200

    Remove users games when account canceled

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+  # DELETE /resource
+  def destroy
+    user = current_user
+    user.games.destroy_all
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed if is_flashing_format?
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
+
 end
