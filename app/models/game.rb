e963305c949af5390c7355bc54bf98f3329ab3a6
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-03-01 00:25:51 +0200

    Use searchkick for game model search

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,6 @@
 class Game < ApplicationRecord
+  searchkick word_start: [:title]
   belongs_to :user
   belongs_to :mooch_user, class_name: 'User', optional: true
   validates :user_id, presence: true
-  def self.search(search)
-    where("title LIKE ?", "%#{search}%") 
-  end
 end
