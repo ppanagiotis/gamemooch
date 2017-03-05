40f6678ecfb2ed382bcc2c755f03216f9b53f080
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-03-05 14:38:54 +0200

    Use helper for games_grid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,3 +26,15 @@ module ApplicationHelper
     end
   end
 end
+
+def games_grid(games)
+  @games.map  {
+    |game| [ {
+      data: { 'img-src': game.url }
+    },
+    if game.user.username? then "<b>#{game.title}</b> <hr> Owner: <b> #{game.user.username} </b>"
+    else  "<b>#{game.title}</b> <hr> Owner: <b> #{game.user.email} </b>"
+    end ,
+    [ 'id' => game.id, 'name' =>  game.title, 'cover' => game.url, 'owner' => game.user.email ] ]
+  }
+end
