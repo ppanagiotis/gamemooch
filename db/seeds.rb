3e947ff60d0c30e986569b0de31f0d7cab516328
Panagiotis Panagiotopoulos <ppanagiwtis@gmail.com>
2017-12-21 18:38:29 +0200

    Use seeds.rb for consoles db table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+platforms = Rails.configuration.platforms.keys
+
+platforms.each do |name|
+  Console.create( name: name )
+end
